011cb8fdd07d92fd172589ba9143d49e0788f979
Ayushi Mohan <mishimohan16@gmail.com>
2020-08-12 19:34:49 +0530

    latest

--- js/app.js
+++ js/app.js
@@ -10,6 +10,17 @@ function SwitchViewToVoiceRoom() {
     window.location.href = 'roomselection.html';
 }
 
+function activate(){
+    document.getElementById("bar").style.backgroundColor="#2d71cf";
+    document.getElementById("btnNext").style.backgroundColor="#2d71cf";
+    document.getElementById("nextB").onclick = function() {SwitchViewToVoiceRoom()};
+}
+function deactivate(){
+    document.getElementById("bar").style.backgroundColor="rgb(182, 182, 182)";
+    document.getElementById("btnNext").style.backgroundColor="rgb(182, 182, 182)";
+    document.getElementById("nextB").onclick = null;
+}
+
 //Storing data to indexedDB with structure
 // recID, recordingName, blob; 
 
--- js/roomselection.js
+++ js/roomselection.js
@@ -1,38 +1,9 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
 var ThemesList = [{ theme: "joker", video: "../assets/mobile/videos/jokertheme.mp4" , outHead: "blah"},
-{ theme: "clouds", video: "../assets/mobile/videos/cloudsthemevideo.mp4" , outHead: "Let out \nyour critical\n inner voices"},
-{ theme: "greenspace", video: "../assets/mobile/videos/greenthemevideo.mp4", outHead: "Be a friend\n to yourself"},
+{ theme: "clouds", video: "../assets/mobile/videos/cloudsthemevideo.mp4" , outHead: "Let out <br>your critical <br>inner voices"},
+{ theme: "greenspace", video: "../assets/mobile/videos/greenthemevideo.mp4", outHead: "Be a friend <br>to yourself"},
 { theme: "lighttunnel", video: "../../assets/videos/lighttunnelthemevideo.mp4" , outHead: "blah"}, //different flow
-{ theme: "cave", image:  "../assets/mobile/cave-1.jpg" , outHead: "Confront the\n enemy within"}, 
-{ theme: "watchingwaves", video: "../assets/mobile/videos/wavesvideo.mp4" , outHead: "Let out the\n frustrations \ninside you"}];
+{ theme: "cave", image:  "../assets/mobile/cave-1.jpg" , outHead: "Confront the <br>enemy within"}, 
+{ theme: "watchingwaves", video: "../assets/mobile/videos/wavesvideo.mp4" , outHead: "Let out the <br>frustrations <br>inside you"}];
 
 function focusToVoiceRoom() {
     var voiceroomscene = document.getElementById("VoiceRoom");
